5ba7f69011963638e39e346230c0407e75f6226f
Garrett Kucinski <garrettryan@doejo.com>
2019-06-09 15:01:17 -0700

    tweak content, update code

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,20 +4,6 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = ({ data }) => {
-  const siteTitle = data.site.siteMetadata.title
-
-  return (
-    <Layout location={this.props.location} title={siteTitle}>
-      <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
-  )
-};
-
-export default NotFoundPage
-
 export const pageQuery = graphql`
   query {
     site {
@@ -27,3 +13,17 @@ export const pageQuery = graphql`
     }
   }
 `
+
+const NotFoundPage = ({ data, location }) => {
+  const { siteMetadata: { title } } = data.site;
+
+  return (
+    <Layout location={location} title={title}>
+      <SEO title="404: Not Found" />
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  )
+};
+
+export default NotFoundPage
